d9d6f35ae798e40b42db71735ba7ec97e32d2063
Đoàn Thị Diễm <diemdt@dc1.gemvietnam.com>
2025-07-24 17:07:28 +0700

    mag cong don 2

--- src/MangCongDon/B51MangCongDon2Chieu.java
+++ src/MangCongDon/B51MangCongDon2Chieu.java
@@ -1,5 +1,46 @@
 package MangCongDon;
 
-public class B51MangCongDon2Chieu {
+import java.util.Scanner;
 
+public class B51MangCongDon2Chieu {
+	public static Scanner sc = new Scanner(System.in) ;
+	public static long [][] pre = new long [1005][1005] ;
+	
+	public static void mang_cong_don_2_chieu(int n, int m, int[][] a) {
+		
+		for( int i = 0; i<= n; i++) {
+			for( int j = 0; j <= m ; j++) {
+				pre[i][j] = 0 ;
+			}
+		}
+		
+		for( int i = 1; i<= n; i++) {
+			for( int j = 1; j <= m ; j++) {
+				pre[i][j] = pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1] + a[i][j] ;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		int n = sc.nextInt(), m = sc.nextInt();
+		
+		int [][] a = new int[1005][1005] ;
+		
+		for( int i = 1 ; i<= n; i++ ) {
+			for( int j = 1 ; j<= m ; j++) {
+				a[i][j] = sc.nextInt() ;
+			}
+		}
+		
+		mang_cong_don_2_chieu(n, m, a);
+		
+	int t = 3; 
+	while( t-- > 0 ) {
+		
+		
+		int x1 = sc.nextInt(), y1 = sc.nextInt(), x2 = sc.nextInt(), y2 = sc.nextInt() ;
+		
+		System.out.println(pre[x2][y2] - pre[x1 - 1][y2] - pre[x2][y1 - 1] + pre[x1-1][y1-1]);
+	}
+	}
 }
--- src/XauKyTu/B59MotSoBaiThaotacVoiString.java
+++ src/XauKyTu/B59MotSoBaiThaotacVoiString.java
@@ -1,7 +1,14 @@
 package XauKyTu;
 
+import java.awt.im.InputSubset;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import javax.xml.stream.events.Characters;
 
@@ -106,7 +113,6 @@ public class B59MotSoBaiThaotacVoiString {
 		for( int i = 0; i < s.length() ; i ++ ) {
 			d[s.charAt(i)]++ ;
 			if( d[s.charAt(i)] == 2) {
-			
 				System.out.println(s.charAt(i));
 				return;
 			} 
@@ -114,6 +120,204 @@ public class B59MotSoBaiThaotacVoiString {
 		
 	 System.out.println("NONE");
 	}
+	
+	public static void chuan_hoa_ngay_sinh() {
+		String s = sc.nextLine() ;
+		
+		if( s.charAt(1) == '/') s = '0' + s ;
+		if( s.charAt(4) == '/') {
+			String s1 = s.substring(0, 3) ;
+			String s2 = s.substring(3) ;
+			s = s1 + "0" + s2 ;
+		}
+		
+		System.out.println(s);
+	}
+	
+	public static void dem_tu_in_hoa() {
+		String s = sc.nextLine() ;
+		
+		String [] list = s.split(" ") ;
+		
+		int cnt = 0;
+		for( int i = 0; i< list.length ; i++) {
+			int isUpper = 1 ;
+			for( int j = 0; j < list[i].length() ; j++ ) {
+				
+				if (!Character.isUpperCase(list[i].charAt(j))){
+					isUpper = 0 ;
+					break;
+				}
+			}
+			if(isUpper == 1 ) {
+				cnt++ ;
+			}
+		}
+		System.out.println(cnt);
+	}
+	
+	public static void liet_ke_tu () {
+		String s = sc.nextLine() ;
+		
+		String [] ls = s.split("[\\\\s.,!?]+") ;
+		
+		int n = 0 ;
+		
+		while(n < ls.length - 1) {
+			System.out.print(ls[n] + " ");
+			n++ ;
+		}
+		System.out.println(ls[n]);
+		
+	}
+	
+	public static String reverse (String s) {
+		char[] cs = s.toCharArray() ;
+		int l = 0, r = cs.length - 1 ;
+		
+		while( l < r ) {
+			char tmp = cs[l] ;
+			cs[l] = cs[r];
+			cs[r] = tmp ;
+			l++; r--;
+		}
+		return new String(cs) ;
+		
+	}
+	
+	public static void tu_chan() {
+		String s = sc.nextLine();
+		
+		String[] ls = s.split(" ");
+		
+		for( int i = 0; i < ls.length ; i++) {
+			if (i % 2 == 0) {
+			    System.out.print(ls[i] + " ");
+			} else {
+			    System.out.print(reverse(ls[i]) + " ");
+			}
+
+		}
+		
+		
+		
+	}
+	
+	public static void sap_xep_ky_tu() {
+		String s = sc.nextLine();
+		
+		String[] ls = s.split(" ");
+		
+		for( int i = 0; i < ls.length ; i++) {
+			
+			char [] tmp = ls[i].toCharArray() ;
+			
+			 Arrays.sort(tmp);
+			 
+			 String res = new String(tmp);
+
+			 System.out.println(res);
+		}
+		
+		
+		
+	}
+	
+	public static void sap_xep_tu() {
+		String s = sc.nextLine();
+		
+		String[] ls = s.split(" ");
+		
+		Arrays.sort( ls, new Comparator<String>() {
+
+			@Override
+			public int compare(String a, String b) {
+				if( a.length() != b.length()) return Integer.compare(b.length(), a.length()) ;
+				else return a.compareTo(b) ;
+			}
+			
+		}
+		
+
+		);
+		for( String x : ls) {
+			System.out.println(x);
+		}
+		
+	}
+	
+	public static void dem_so_luong_tu_khac_nhau() {
+		String s = sc.nextLine() ;
+		s = s.toLowerCase() ;
+		
+		String[] ls = s.split(" ") ;
+		
+		Set<String> se = new HashSet<String>() ;
+		
+		
+		for( int i = 0; i < ls.length ; i++ ) {
+			
+			se.add(ls[i]) ;
+		
+		}
+		
+		System.out.println(se.size());
+	}
+	
+	public static void cac_tu_thuan_nghich() {
+		String s = sc.nextLine() ;
+		
+		String [] ls = s.split(" ") ;
+		
+		List<String> res = new ArrayList<String>();
+		 
+		for( int i = 0 ; i < ls.length ; i++) {
+			if( is_thuan_nghich(ls[i]) == 1) {
+				res.add(ls[i]);
+			}
+		}
+		
+//		Collections.sort(res);
+		
+		Collections.sort(res, new Comparator<String>() {
+
+			@Override
+			public int compare(String a, String b) {
+				if( a.length() != b.length()) return b.compareTo(a) ;
+				else return b.compareTo(a) ;
+				
+			}
+			
+			
+		}) ;
+		
+		for( String x : res) {
+			System.out.println(x);
+		}
+		
+	}
+	
+	public static int is_thuan_nghich(String s) {
+
+		char [] cs = s.toCharArray() ;
+		
+		int l = 0, r = cs.length - 1;
+		
+		while( l < r ) {
+			char tmp = cs[l]  ;
+			cs[l] = cs[r] ;
+			cs[r] = tmp ;
+			
+			l++; r--;
+		}
+		
+		String rev = new String(cs) ;
+		
+		if( s.equals(rev)) return 1;
+		else return 0;
+	}
+	
+	
 	public static void main(String[] args) {
 
 		int t = 3 ;
@@ -125,7 +329,20 @@ public class B59MotSoBaiThaotacVoiString {
 //			hai_8_tech_va_teo();
 			
 //			chen_vao_vi_tri_k();
-			ky_tu_dau_tien_bi_lap();
+//			ky_tu_dau_tien_bi_lap();
+//			chuan_hoa_ngay_sinh();
+			
+//			dem_tu_in_hoa();
+			
+//			liet_ke_tu();
+//			tu_chan();
+			
+//			sap_xep_ky_tu();
+//			sap_xep_tu();
+			
+//			dem_so_luong_tu_khac_nhau();		
+			
+			cac_tu_thuan_nghich();
 		}
 	}
 }
