50b8a9f65c9e3ff9bd12fd7dae525f0fad91d2aa
Đoàn Thị Diễm <diemdt@dc1.gemvietnam.com>
2025-07-28 17:11:58 +0700

    ___

--- src/HaiConTro/HaiConTro.java
+++ src/HaiConTro/HaiConTro.java
@@ -7,7 +7,9 @@ import java.util.Scanner;
 public class HaiConTro {
 	public static Scanner sc = new Scanner(System.in) ;
 	
-//	Cho 2 mảng A[] và B[] có N và M phần tử đã được sắp xếp theo thứ tự tăng dần, nhiệm vụ của bạn là đối với mỗi phần tử trong mảng B[], hãy đếm xem trong mảng A[] có bao nhiêu phần tử nhỏ hơn nó.
+//	Cho 2 mảng A[] và B[] có N và M phần tử đã được sắp xếp theo thứ tự tăng dần, 
+//	nhiệm vụ của bạn là đối với mỗi phần tử trong mảng B[], 
+//	hãy đếm xem trong mảng A[] có bao nhiêu phần tử nhỏ hơn nó.
 //	6 7
 //	1 2 3 5 7 10
 //	2 2 2 2 2 2 2
@@ -34,7 +36,7 @@ public class HaiConTro {
 				System.out.print(i + " ");
 				j++;
 			} else {
-				j++ ;
+				i++ ;
 			}
 		}
 		
@@ -44,10 +46,62 @@ public class HaiConTro {
 		}
 	}
 	
+	
+//	Cho 2 mảng A[] và B[] có N và M phần tử đã được sắp xếp theo thứ tự tăng dần, 
+//	nhiệm vụ của bạn là đối với mỗi phần tử trong mảng B[], 
+//	hãy đếm xem trong mảng A[] có bao nhiêu cặp a[i] b[j] bang nhau 
+	public static void dem_so_cap_bang_nhau() {
+		int n = sc.nextInt(), m = sc.nextInt() ;
+		
+		List<Integer> a = new ArrayList<Integer>(n) ;
+		List<Integer> b = new ArrayList<Integer>(m) ;
+		
+		for( int i = 0; i < n; i++) {
+			a.add(sc.nextInt());
+		}
+		
+		for( int i = 0; i < m; i++) {
+			b.add(sc.nextInt());
+		}
+		
+		int ans = 0;
+		int i = 0, j = 0;
+		while( i < n && j < m ) {
+			if( a.get(i) == b.get(j)) {
+				int cnt1 = 0;
+				while( a.get(i) == b.get(j)) {
+					cnt1++ ;
+					i++;
+				}
+				
+				int cnt2 = 0;
+				while( a.get( i - 1 ) == b.get(j)) {
+					cnt2++ ;
+					j++;
+				}
+				
+				ans += 1l * cnt1 * cnt2;
+				
+				
+			} else if( a.get(i) < b.get(j)) {
+				i++ ;
+			} else {
+				 j++ ;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+	
+	
+//	Cho mang A co n phan tu, tim day ans co do dai lon nhat ma tong <= s
+//	11 10
+//	3 1 4 1 5 3 4 1 5 2 2 -> 4
+	
 	public static void main(String[] args) {
 		int t = sc.nextInt() ;
 		while( t-- > 0) {
-			
+		dem_so_cap_bang_nhau();	
 		}
 	}
 
